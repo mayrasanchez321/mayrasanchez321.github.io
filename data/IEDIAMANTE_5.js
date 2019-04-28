da8d6f672f63dbed86cfd25fd8e1f9f088b56a62
mayrasanchez321 <48108286+mayrasanchez321@users.noreply.github.com>
2019-04-28 10:24:11 -0500

    Add files via upload

---
+++ data/IEDIAMANTE_5.js
@@ -0,0 +1,8 @@
+var json_IEDIAMANTE_5 = {
+"type": "FeatureCollection",
+"name": "IEDIAMANTE_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -76.502694479152069, 3.418919587517578 ], [ -76.50427123422368, 3.420514892648848 ], [ -76.503065480345384, 3.421609346169138 ], [ -76.501470175214124, 3.420069691216866 ], [ -76.502694479152069, 3.418919587517578 ] ] ] ] } }
+]
+}
